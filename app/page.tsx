68e44ec9a2f0d4156d8798b99c6a99d068765fc1
Guilherme <122334549+keloy20@users.noreply.github.com>
2026-01-13 02:32:50 -0300

    teste hidratação raiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from "next/navigation";
+
+"use client";
 
 export default function Home() {
-  redirect("/login");
+  return (
+    <div>
+      <h1>TESTE RAIZ</h1>
+      <button onClick={() => alert("RAIZ CLICOU")}>
+        BOTÃO RAIZ
+      </button>
+    </div>
+  );
 }
